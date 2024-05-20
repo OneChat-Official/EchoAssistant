3f46b239e3091f17e4fc872c8a530bc1052598e9
Kyle Moore <kylem70698@gmail.com>
2024-05-20 09:51:43 -0700

    fixing bugs

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# ChatGPTClone
-A ChatGPT clone
+# EchoAssistant
+
+A simple echo assistant. See the OneChat quickstart for instructions on how to deploy this assistant.
--- app.py
+++ app.py
@@ -1,24 +1,8 @@
 from fastapi import FastAPI
-import os
-from openai import OpenAI
-from dotenv import load_dotenv
 from models import NewMessageContext
-
-load_dotenv()
 
 app = FastAPI()
 
-client = OpenAI(
-    api_key=os.environ.get("OPENAI_API_KEY")
-)
-model = os.environ.get("OPENAI_MODEL")
-
 @app.post("/chat/")
 async def chat(context: NewMessageContext):
-
-    completion = client.chat.completions.create(
-        model=model,
-        messages=context.messages
-    )
-
-    return {"response": completion.choices[0].message.content}
+    return {"response": context.latest_message.content}
